2bc652e594f51bf94aacd25dbe0a14e024b01403
zhang jiaqing <54712081+icey-zhang@users.noreply.github.com>
2023-02-16 17:54:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ def test(data,
          save_txt=False,  # for auto-labelling
          save_hybrid=False,  # for hybrid auto-labelling
          save_conf=False,  # save auto-label confidences
-         plots=False,
+         plots=True,
          wandb_logger=None,
          compute_loss=None,
          is_coco=False):
@@ -346,7 +346,7 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/small_EDSR_fold1.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='small_EDSR_fold1.pt', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default='data/SRvedai.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=1, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
